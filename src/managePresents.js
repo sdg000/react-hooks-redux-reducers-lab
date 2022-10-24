1ce1a8b99480777ce6c2071cac3a4d07287ef3d3
Seth David Gyimah <esec26@gmail.com>
2022-10-24 16:37:37 +0000

    completed lab requirements

--- src/managePresents.js
+++ src/managePresents.js
@@ -1,5 +1,16 @@
 const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
-  // your code here
+
+  // updating state based on action type
+  switch (action.type){
+
+    // if action is type: "presents/increase", increment numberOfPresents by 1.
+    case "presents/increase":
+      return {numberOfPresents: state.numberOfPresents + 1}
+
+      // if no action type is matched, set default numberOfPresents = 0
+    default:
+      return {numberOfPresents: 0}
+  }
 }
