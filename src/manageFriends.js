4614b76fcfdb85d049162607b7820c90af666d4b
Seth David Gyimah <esec26@gmail.com>
2022-10-24 16:37:27 +0000

    commit tree 52880049cde6700cae5d58e2aed0607d01a7b560

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,32 @@
+// initial state should be an object with a key, friends, set to an empty array.
+// state = {friends: []}
+
 export function manageFriends(state, action) {
-  // your code here
+
+  // updating state based on action type
+  switch (action.type) {
+
+  // if action is type: "friends/add", add new friend Object to state Array by;
+      // -returning friends Object with value:
+      // (old friends objects which has been copied using spread operator
+      //  plus new friend object added using (action.payload))
+
+  case "friends/add":
+    return {
+      friends: [...state.friends, action.payload]
+    }
+
+  // if action is type: "friends/remove", find and remove friend from friends Object using ID
+  // filter friends Object and return all objects except one mathing id of (friend to be removed)
+  
+  case "friends/remove":
+    return {
+      friends: state.friends.filter((friend) => friend.id !== action.payload)
+    }
+
+  // if no action type is matched, return all friends
+  default:
+    return state
+  }
+
 }
